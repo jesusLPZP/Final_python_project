dd68a66eb7b29beb786a5593729702b7ff6847a1
jesusLPZP <jesusl012345678900000@gmail.com>
2025-11-11 09:38:57 -0700

    trying to work on time

--- finalproject.py
+++ finalproject.py
@@ -1,7 +1,10 @@
 import random
+#imported time
+import time
 a = int(input("Enter a number to be the range of the questions: "))
 score = 0
 total = ""
+start_time = time.time()
 while total == "":
     number1 = random.randint(1, a)
     number2 = random.randint(1, a)
@@ -14,6 +17,11 @@ while total == "":
         total = ""
         
     else:
+        end_time = time.time()
+        elapsed_time = end_time - start_time
         print("Incorrect. The correct answer is", total)
-        print("You got ", score , "questions correct.")
-    
+        print("You got ", score , "questions correct in", round(elapsed_time,2), "seconds.") 
+        #gotta work on this part, either to fix or remove it
+        #try to focus on average score per second or if i cant change it to per minute
+        average_score = score / (elapsed_time/60)
+        print("you got an average of", average_score, "questions per second.")
